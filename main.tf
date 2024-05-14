12c0134af5504ed77894281bf6f785e6aa39c69d
Aliaksandr Batsin <aliaksandr_batsin@epam.com>
2024-05-14 22:26:04 +0400

    tst

--- main.tf
+++ main.tf
@@ -60,12 +60,3 @@ resource "azurerm_linux_virtual_machine" "Ubuntu" {
   }
 }
 
-
-<<<<<<< HEAD
-=======
-resource "azurerm_resource_group" "RG2" {
-  name     = "resource-group2"
-  location = "West Europe"
-}
-
->>>>>>> 9567ea3 (add)
